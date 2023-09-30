346ba3dd643ac8a0db00507ca65b17fd6b49d9ee
Beetroot Paul <paul@beetrootpaul.com>
2023-10-01 01:51:32 +0200

    harpoon

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- TODO: code completion
 -- TODO: consider disabling ligatures: `<leader>=` is an example of `>=` ligature for `=`j
 -- TODO: learn how to make line into a code comment
 -- TODO: split config into separate dirs/files under lua/brp/init.lua & lua/brp/something/lua (remember the require from the inside nested dir still needs to specify full path with dots)
@@ -93,6 +94,22 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		-- https://github.com/ThePrimeagen/harpoon
+		"ThePrimeagen/harpoon",
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function() 
+			local mark = require("harpoon.mark")
+			local ui = require("harpoon.ui")
+
+			vim.keymap.set("n", "<leader>m", mark.add_file)
+			vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+			vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+			vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+			vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+			vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+		end
+	}
 })
 
 -- open file explorer (`pv` = project view)
