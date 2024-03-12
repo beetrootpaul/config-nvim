c0fa59ebab62a8d3111dd592444399058b934365
Beetroot Paul <paul@beetrootpaul.com>
2024-03-12 18:23:05 +0100

    experiment with kanagawa theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,16 @@
 -- https://github.com/rose-pine/neovim
 return {
+  
   "rose-pine/neovim",
-  lazy = false,
+  --"rebelot/kanagawa.nvim",
+
   name = "rose-pine",
+  --name = "kanagawa",
+
+  lazy = false,
   priority = 1000,
   config = function()
     vim.cmd("colorscheme rose-pine-moon")
+    --vim.cmd("colorscheme kanagawa")
   end,
 }
