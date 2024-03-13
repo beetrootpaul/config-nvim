e886deb796d0c2a9fea34d4175aeda661248072c
Beetroot Paul <paul@beetrootpaul.com>
2024-03-14 00:36:19 +0100

    none-ls, stylua, prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,39 +1,69 @@
 return {
 
-  -- https://github.com/williamboman/mason.nvim
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	-- https://github.com/williamboman/mason.nvim
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- https://github.com/williamboman/mason-lspconfig.nvim
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-        },
-      }
-    end,
-  },
+	-- https://github.com/williamboman/mason-lspconfig.nvim
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"stylua",
+				},
+			})
+		end,
+	},
 
-  -- https://github.com/neovim/nvim-lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
+	-- https://github.com/neovim/nvim-lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup ({})
-      lspconfig.tsserver.setup ({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+
+	-- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier",
+					"stylua",
+				},
+			})
+		end,
+	},
+
+	-- https://github.com/nvimtools/none-ls.nvim
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
-
